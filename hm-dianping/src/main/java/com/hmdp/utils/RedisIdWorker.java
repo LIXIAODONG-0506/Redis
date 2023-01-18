2055748f3a60e3f23a946b044f7b6dfc3273c619
栗晓东 <a123456789>
2023-01-18 17:06:14 +0800

    秒杀：全局ID生成器

---
+++ hm-dianping/src/main/java/com/hmdp/utils/RedisIdWorker.java
@@ -0,0 +1,48 @@
+package com.hmdp.utils;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @author dong
+ * @since 2023/1/18 16:30
+ */
+@Component
+public class RedisIdWorker {
+
+    /**
+     * 开始时间戳
+     */
+    private static final long BEGIN_TIMESTAMP = 1640995200L;
+
+    /**
+     * 序列号的位数
+     */
+    private static final int COUNT_BITS = 32;
+
+    private StringRedisTemplate stringRedisTemplate;
+
+    public RedisIdWorker(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    public long nextId(String keyPrefix){
+        // 1.生成时间戳
+        LocalDateTime now = LocalDateTime.now();
+        long l = now.toEpochSecond(ZoneOffset.UTC);
+        long timestamp = l - BEGIN_TIMESTAMP;
+
+        // 2.生成序列号
+        // 2.1.获取当前日期，精确到天
+        String date = now.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+        // 2.2.自增长
+        Long increment = stringRedisTemplate.opsForValue().increment("icr" + keyPrefix + ":" + date);
+
+        // 3.拼接并返回
+        return timestamp << COUNT_BITS | increment;
+    }
+}
--- hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -2,16 +2,44 @@ package com.hmdp;
 
 import com.hmdp.service.IShopService;
 import com.hmdp.service.impl.ShopServiceImpl;
+import com.hmdp.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
 
     @Resource
     private ShopServiceImpl shopServiceImpl;
+
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
+    private ExecutorService es = Executors.newFixedThreadPool(500);
+    @Test
+    public void testIdWorker() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(300);
+        Runnable task = () -> {
+            for (int i = 0; i < 100; i++){
+                long order = redisIdWorker.nextId("order");
+                System.out.println("id=" + order);
+            }
+            latch.countDown();
+        };
+        long begin = System.currentTimeMillis();
+        for(int i = 0; i < 300; i++){
+            es.submit(task);
+        }
+        latch.await();
+        long end = System.currentTimeMillis();
+        System.out.println("time = " + (end - begin));
+
+    }
 
     @Test
     public void test() throws InterruptedException {
